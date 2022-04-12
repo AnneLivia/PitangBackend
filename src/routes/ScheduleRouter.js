515149b3c2fbbf3c9d6718413a18dfee32dd01b2
Anne Livia <annelivia16@gmail.com>
2022-04-12 13:37:53 -0300

    validation middleware was added (PUT and POST)

--- src/routes/ScheduleRouter.js
+++ src/routes/ScheduleRouter.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import ScheduleController from '../controllers/ScheduleController.js';
+import validationMiddleware from '../middlewares/validation.middleware.js';
+import { postSchema, putSchema } from '../validators/scheduleSchema.js';
 
 const router = Router();
 
 const scheduleController = new ScheduleController();
 
 router.get('/schedules', scheduleController.index.bind(scheduleController));
-router.get('/schedules/:id', scheduleController.getOne.bind(scheduleController));
-router.post('/schedules', scheduleController.store.bind(scheduleController));
-router.put('/schedules/:id', scheduleController.update.bind(scheduleController));
+router.post('/schedules', validationMiddleware(postSchema), scheduleController.store.bind(scheduleController));
+router.put('/schedules/:id', validationMiddleware(putSchema), scheduleController.update.bind(scheduleController));
 router.delete('/schedules/:id', scheduleController.delete.bind(scheduleController));
 
 export default router;
