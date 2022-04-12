18ec31bdc24eb4306b92f517ce454a95baa9a296
Anne Livia <annelivia16@gmail.com>
2022-04-12 12:03:43 -0300

    Schedule controller created

---
+++ src/controllers/ScheduleController.js
@@ -0,0 +1,17 @@
+import ScheduleModel from '../models/ScheduleModel.js';
+
+class ScheduleController {
+  async index(req, res) {
+    res.json({ message: 'working...' });
+  }
+
+  async getOne(req, res) {}
+
+  async store(req, res) {}
+
+  async update(req, res) {}
+
+  async delete(req, res) {}
+}
+
+export default ScheduleController;
